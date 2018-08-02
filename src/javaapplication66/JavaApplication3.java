4da0929313b547feca3a14825793b66680e8fdfa
mohammad Qasim <mqasim2017@my.fit.edu>
2018-08-03 01:13:47 +0300

    final project
    final project

---
+++ src/javaapplication66/JavaApplication3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication66;
+
+
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Main_Frame mf= new Main_Frame();
+        
+        mf.setVisible(true);
+    }
+    
+}
